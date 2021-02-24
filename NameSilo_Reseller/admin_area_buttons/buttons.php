653a26e92fcbc072d3751a4d986ed37f82f28e3e
ThosHost <39840976+TheKenzo@users.noreply.github.com>
2021-02-24 16:28:05 +0200

    Add files via upload

---
+++ NameSilo_Reseller/admin_area_buttons/buttons.php
@@ -0,0 +1,8 @@
+<?php
+if(addon_activated('NameSilo_Reseller') == "YES")
+{
+	?>
+	<a class="btn btn-warning" style="margin-top: 5px; color: white;" href="client_area?admin_area=1&addon_use=NameSilo_Reseller"><i class="fas fa-globe"></i> <b style="font-family: Muli, sans-serif;">NameSilo Reseller</b></a>
+	<?php
+}
+?>
